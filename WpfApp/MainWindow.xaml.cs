423d6a7c90b8acc4e1cc38d8e45417dc10c63ca1
Artem Petselia <mrtimego@ukr.net>
2021-12-20 12:40:34 +0200

    replica fixed

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Windows;
 using WpfApp.Models;
 using WpfApp.Views;
@@ -14,8 +15,27 @@ namespace WpfApp
         {
             InitializeComponent();
 
+            UniversityContext main = UniversityContext.Instance;
+
             UniversityContext replica = new UniversityContext("Host=localhost;Port=5432;Database=university_replica;Username=postgres;Password=Artik2003");
             replica.CreateSubscription();
+
+            if (!main.Students.Any() &&
+                !main.Tests.Any() &&
+                !main.Groups.Any() &&
+                !main.Teachers.Any() &&
+                !main.Subjects.Any() &&
+                !main.Grades.Any())
+            {
+                main.Students.AddRange(replica.Students);
+                main.Tests.AddRange(replica.Tests);
+                main.Grades.AddRange(replica.Grades);
+                main.Groups.AddRange(replica.Groups);
+                main.Subjects.AddRange(replica.Subjects);
+                main.Teachers.AddRange(replica.Teachers);
+
+                main.SaveChanges();
+            }
 
             UniversityContext.Instance.Students.Load();
             UniversityContext.Instance.Grades.Load();
